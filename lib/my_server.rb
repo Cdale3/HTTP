cdb52d0083f51fee36dff17b6473b355f0df694e
Christopher Dale <epiphone2903@hotmail.com>
2017-02-13 15:41:34 -0700

    got too iteration 2, want to rewrite it into classes where it makes more sense

---
+++ lib/my_server.rb
@@ -0,0 +1,20 @@
+class MyServer
+
+  def initialize
+    #variables that will maintain state
+    #counter
+    #TCPServer
+
+  end
+
+  def connect
+    #pick up incoming request / open server
+    #which path? - get request lines
+    #clien = tcp_server.accept
+  end
+
+  def respond
+    #server body
+  end
+
+end
