1e90212c994152fbd768bf2dd2cc0d1485a6b48f
Christopher Dale <epiphone2903@hotmail.com>
2017-02-13 23:02:21 -0700

    making changes towards classes, getting response and server set up

---
+++ lib/main.rb
@@ -0,0 +1,26 @@
+require 'socket'
+require 'faraday'
+require './lib/server.rb'
+require './lib/request.rb'
+require './lib/response.rb'
+
+class Main
+  attr_reader :server, :connection
+  def initialize
+    @server = Server.new
+    #server will make a new instance of server
+    @connection = server.setup_connection
+  end
+
+    def experiment
+      request = Request.new
+      request.read_request(connection)
+      request.display_request
+      response = Response.new
+      response.build_response(connection, request.request_lines)
+      server.close_connection
+    end
+end
+
+main = Main.new
+main.experiment
