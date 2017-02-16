949b293f710022526961200b57af3c2dd0a213a8
Christopher Dale <epiphone2903@hotmail.com>
2017-02-15 22:12:41 -0700

    iteration 2, when in a class server stays open for 1 request, when not in a class server is available the entire time

--- lib/server.rb
+++ lib/server.rb
@@ -1,56 +1,66 @@
 require 'socket'
-require 'pry'
 
+# class Server
 
-tcp_server = TCPServer.new(9292)
-counter = 0
-while client = tcp_server.accept
-  counter += 1
-
-  #Snippet to keep reading stream until input is a blank line. Stores all requests in "request_lines" array
   puts "Ready for a request"
-  request_lines = []
 
-  while line = client.gets and !line.chomp.empty?
-    request_lines << line.chomp
+  tcp_server = TCPServer.new(9292)
+  hello_hits = 0
+  total_requests = 0
+
+  loop do
+    client = tcp_server.accept
+
+    request_lines = []
+    while line = client.gets and !line.chomp.empty?
+      request_lines << line.chomp
+    end
+
+
+    puts "Sending response."
+
+    def diagnostics(request_lines)
+      verb_path_protocol_heading = request_lines.first.split
+      host_heading = request_lines[1].split(":").map(&:strip)
+      host_heading = request_lines[1].split(":").map{|line| line.strip}
+      accept_heading = request_lines[6].split
+      [
+        "<pre>",
+        "Verb: #{verb_path_protocol_heading[0]}",
+        "Path: #{verb_path_protocol_heading[1]}",
+        "Protocol: #{verb_path_protocol_heading[2]}",
+        "Host: #{host_heading[1]}",
+        "Port: #{host_heading[2]}",
+        "Origin: #{host_heading[1]}",
+        "Accept: #{accept_heading[1]}",
+        "</pre>"
+      ].join("\n")
+    end
+
+    case request_lines.first.split[1]
+    when "/"
+      total_requests += 1
+      client.puts "<html><head></head><body>#{diagnostics(request_lines)}</body></html>"
+    when "/hello"
+      total_requests += 1
+      hello_hits += 1
+      output = "Hello, World! (#{hello_hits})"
+      client.puts "<html><head></head><body>#{output}</body></html>"
+      client.puts diagnostics(request_lines)
+    when "/datetime"
+      total_requests += 1
+      output = "#{Time.now.strftime('%l:%m%p on %A, %B %e, %Y')}"
+      client.puts "<html><head></head><body>#{output}</body></html>"
+      client.puts diagnostics(request_lines)
+    when "/shutdown"
+      total_requests += 1
+      output = "Total Requests: #{total_requests}"
+      client.puts "<html><head></head><body>#{output}</body></html>"
+      client.puts diagnostics(request_lines)
+      break
+    end
+
+    client.close
+    puts "\nResponse complete, exiting."
   end
-
-  puts "Got this request:"
-  puts request_lines.inspect
-  puts "Sending response."
-  response = "<pre>" + request_lines.join("\n") + "</pre>"
-  verb = request_lines[0].split[0]
-  path = request_lines[0].split[1]
-  protocol = request_lines[0].split[2]
-  host = request_lines[1].split(":")[1].lstrip
-  port = request_lines[1].split(":")[2]
-  origin = host
-  accept = request_lines[-3].split[1]
-
-#binding.pry
-  diagnostics = <<END_OF_DIAGNOSTICS
-<pre>
-  Verb: #{verb}
-  Path: #{path}
-  Protocol: #{protocol}
-  Host: #{host}
-  Port: #{port}
-  Origin: #{origin}
-  Accept: #{accept}
-</pre>
-END_OF_DIAGNOSTICS
-
-  output = "<html><head></head><body>Hello World! Counting:#{counter} #{response} #{diagnostics}</body></html>"
-  headers = ["http/1.1 200 ok",
-            "date: #{Time.now.strftime('%a, %e %b %Y %H:%M:%S %z')}",
-            "server: ruby",
-            "content-type: text/html; charset=iso-8859-1",
-            "content-length: #{output.length}\r\n\r\n"].join("\r\n")
-  client.puts headers
-  client.puts output
-
-  #close up server
-  puts ["Wrote this response:", headers, output].join("\n")
-end
-client.close
-puts "\nResponse complete, exiting."
+# end
